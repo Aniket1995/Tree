08beee7bcbe39765d06d16b5094369991da418f2
Aniket1995 <abhanavase@gmail.com>
2022-08-18 21:55:56 +0530

    BST

--- BST/correctBST_When2_Nodes_Swapped.py
+++ BST/correctBST_When2_Nodes_Swapped.py
@@ -1,3 +1,7 @@
+# just take nodes in inorder list, check the swaped elements in frist go. correct the array and restore the tree
+# swap the data of the nodes that it.
+# space O(n) time o(n). This solution goes over the array twise in worst case and travrse tree twise. 
+
 def inorder(root, in_, set_):
     if(root == None):
         return 
@@ -7,7 +11,6 @@ def inorder(root, in_, set_):
     else:
         in_.append(root.data)
     inorder(root.right, in_, set_)
-    
     
     
 #Function to fix a given BST where two nodes are swapped.  
@@ -34,4 +37,39 @@ class Solution:
         in_[i],in_[j] = in_[j],in_[i]
         
         inorder(root,in_,True)
+
+#2 solution
+# just take nodes in inorder list, check the elems in one go using (f)irst, (m)id, (l)ast
+# in case ajacent vals f and m will be present else f and l
+# swap the data of the nodes that it.
+# space O(n) time o(n)
+
+def inorder(root, in_):
+    if(root == None):
+        return 
+    inorder(root.left, in_)
+    in_.append(root)
+    inorder(root.right, in_)
+    
+#Function to fix a given BST where two nodes are swapped.  
+class Solution:
+    def correctBST(self, root):
+        in_=[]
+        inorder(root,in_)
+        len_=len(in_)
+        f=None
+        m=None
+        l=None
         
+        for i in range(len_-1):
+            if(in_[i].data > in_[i+1].data):
+                if(not f):
+                    f=in_[i]
+                    m=in_[i+1]
+                else:
+                    l=in_[i+1]
+        
+        if(f and l):
+            f.data,l.data = l.data,f.data
+        elif(f and m):
+            f.data,m.data = m.data,f.data
